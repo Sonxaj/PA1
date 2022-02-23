5dc82775d266dafd17f4241f9e4ba1a8f3480b93
Sonxaj <LT2SCYKq9RaNhnq>
2022-02-23 16:49:34 -0500

    Initial commit

---
+++ src/VigenereCipherEncryptor/pa01.java
@@ -0,0 +1,50 @@
+/*=============================================================================
+| Assignment: pa01 - Encrypting a plaintext file using the Vigenere cipher
+|
+| Author: Jonas Turner
+| Language: Java
+|
+| To Compile: javac pa01.java
+| gcc -o pa01 pa01.c
+| g++ -o pa01 pa01.cpp
+|
+| To Execute: java -> java pa01 kX.txt pX.txt
+| or c++ -> ./pa01 kX.txt pX.txt
+| or c -> ./pa01 kX.txt pX.txt
+| where kX.txt is the keytext file
+| and pX.txt is plaintext file
+|
+| Note: All input files are simple 8 bit ASCII input
+|
+| Class: CIS3360 - Security in Computing - Spring 2022
+| Instructor: McAlpin
+| Due Date: Sunday, February 26, 2022
+|
++=============================================================================*/
+
+package VigenereCipherEncryptor;
+
+public class pa01 {
+
+    public static void main(String[] args) {
+
+        // read file from script
+
+        // read in plaintext
+
+        // apply cipher algorithm
+
+        // write ciphertext
+
+        // profit
+    }
+}
+
+/*=============================================================================
+| I Jonas Turner (4852668) affirm that this program is
+| entirely my own work and that I have neither developed my code together with
+| any another person, nor copied any code from any other person, nor permitted
+| my code to be copied or otherwise used by any other person, nor have I
+| copied, modified, or otherwise used programs created by others. I acknowledge
+| that any violation of the above terms will be treated as academic dishonesty.
++=============================================================================*/
