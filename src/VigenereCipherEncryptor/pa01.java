67ef9bd88fe11d7085e80760f0f081d66d70b790
Jonas Turner <jonasturner12@gmail.com>
2022-02-24 23:33:06 -0500

    Added Vigenere Cipher Algorithm and other methods

--- src/VigenereCipherEncryptor/pa01.java
+++ src/VigenereCipherEncryptor/pa01.java
@@ -30,14 +30,64 @@ public class pa01 {
 
         // read file from script
 
-        // read in plaintext
+        // create key
+
+        // read plaintext and pad with x
 
         // apply cipher algorithm
 
-        // write ciphertext
+        // print
 
         // profit
+
+
+        char a = 'a';
+        char b = 'b';
+
+
+
+        int c = (charToInt(a) + charToInt(b)) % 26;
+
+        System.out.println(c);
     }
+
+    public String vigenereCipherAlg(String plaintext, String key){
+
+        StringBuilder result = new StringBuilder();
+
+        char[] plaintextTemp = plaintext.toCharArray();
+        char[] keyTemp = key.toCharArray();
+
+        int keyPos = 0;
+
+        for (char c:plaintextTemp) {
+            int cipherChar = charToInt(keyTemp[keyPos]) + charToInt(c);
+            result.append(intToChar(cipherChar));
+
+            if(keyPos == (key.length() - 1))
+                keyPos = 0;
+            else
+                keyPos++;
+        }
+
+        return result.toString();
+    }
+
+    public static int charToInt(char input){
+        return (input - 97);
+    }
+
+    public static char intToChar(int input){
+        return (char)(input + 97);
+    }
+
+    public boolean isCharacter(char input){
+        // check if current character is only alphabet
+        // i.e., ASCII is between 97 (a) and 122 (z) OR 65 (A) and 90 (Z)
+        return (input >= 65 && input <= 90) || (input >= 97 && input <= 122);
+    }
+
+
 }
 
 /*=============================================================================
